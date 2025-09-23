daf99d935aa8f5ac9519c61e9866785b5a1aee81
charmiduoc <dei.velasquez@duocuc.cl>
2025-09-23 20:57:28 -0300

    agregar productos

---
+++ Assets/js/productos.js
@@ -0,0 +1,10 @@
+// Productos “de mentira” para la demo
+window.PRODUCTOS = [
+    { id: 1, nombre: "Tomates",     precio: 1200, vendedor: "Juan Pérez",    categoria: "Verduras" },
+    { id: 2, nombre: "Papas",       precio: 900,  vendedor: "Ana Torres",    categoria: "Verduras" },
+    { id: 3, nombre: "Lechugas",    precio: 500,  vendedor: "Luis Rojas",    categoria: "Verduras" },
+    { id: 4, nombre: "Zanahorias",  precio: 700,  vendedor: "Paula Díaz",    categoria: "Verduras" },
+    { id: 5, nombre: "Manzanas",    precio: 1300, vendedor: "Kathy Muñoz",   categoria: "Frutas"   },
+    { id: 6, nombre: "Naranjas",    precio: 1100, vendedor: "Pedro Soto",    categoria: "Frutas"   },
+  ];
+  
